05549676ffa431c9abbae1422e9b5723f921283d
Adam Grover <adamggrover@gmail.com>
2022-08-31 14:19:05 +0100

    Add js hover effect to new news cards

---
+++ main.js
@@ -0,0 +1,29 @@
+//news card section effects
+
+//mouseOff
+function opacityFunction(){
+    document.getElementById("overlay").style.opacity = '1';
+}
+
+function zIndexFuction(){
+    document.getElementById("news-card-body").style.zIndex = '1';
+}
+
+function linkColorFunction(){
+    document.getElementById("news-card-body").style.color = 'white';
+}
+
+
+//mouseOut
+
+function opacityFunctionR(){
+    document.getElementById("overlay").style.opacity = '0';
+}
+
+function zIndexFuctionR(){
+    document.getElementById("news-card-body").style.zIndex = '0';
+}
+
+function linkColorFunctionR(){
+    document.getElementById("news-card-body").style.color = 'black';
+}
