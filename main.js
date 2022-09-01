1a8420add16ea61cb3cbfec419a65944ef723b78
Adam Grover <adamggrover@gmail.com>
2022-09-01 12:57:56 +0100

    Edit announcement box reveal function

--- main.js
+++ main.js
@@ -9,6 +9,13 @@ function announcementClose() {
     }
   }
 
+  function announcementReveal() {
+    var x = document.getElementById("announcement-wrapper");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } 
+  }
+
 
 //news card section effects
 
